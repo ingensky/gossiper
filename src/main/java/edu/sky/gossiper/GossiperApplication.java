f21143fbe451b78648dc9abd0513ef76119da016
LazyDoge <lazywoolf@gmail.com>
2018-10-20 17:27:38 +0300

    Initial commit

---
+++ src/main/java/edu/sky/gossiper/GossiperApplication.java
@@ -0,0 +1,12 @@
+package edu.sky.gossiper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GossiperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GossiperApplication.class, args);
+	}
+}
